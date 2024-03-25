ef4db8eb4428214181f6ed5572b0f882813dadb0
Max <roosmaxm@arcada.fi>
2024-03-25 15:02:37 +0200

    Lektion 2

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,10 @@
 package fi.arcada.codechallenge;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +12,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView outputText;
+    EditText inputText;
+    double [] values = {12, 3, 53.2 ,7, 65, 55, 43, 21};
+    TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.outText);
+        inputText = findViewById(R.id.inputText);
+        result = findViewById(R.id.result);
+
+
+        outputText.setText("Min app fungerar");
+
+
+    }
+    public void buttonHandler(View view){
+        outputText.setText(inputText.getText().toString());
+    }
+    public void calculator(View view){
+        double average = calculate();
+        result.setText(String.format("%.2f", average));
+    }
+    public double calculate() {
+        double sum = 0;
+        for (double value : values) {
+            sum += value;
+        }
+        return sum / values.length;
     }
 }
