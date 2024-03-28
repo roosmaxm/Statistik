76ce4b2b3e73b50d18f26b84339240de47387065
Max <roosmaxm@arcada.fi>
2024-03-28 15:27:25 +0200

    Lektion 3

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -14,15 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView outputText;
     EditText inputText;
-    double [] values = {12, 3, 53.2 ,7, 65, 55, 43, 21};
+    double [] testData = {12, 3, 53.2 ,7, 65, 55, 43, 21};
+    ArrayList<Double> values = new ArrayList<>();
     TextView result;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         inputText = findViewById(R.id.inputText);
         result = findViewById(R.id.result);
 
+        //VI fyller vår arraylist med värdena från testData
+        for (double value : testData){
+            values.add(value);
+        }
+
 
         outputText.setText("Min app fungerar");
 
@@ -41,8 +47,13 @@ public class MainActivity extends AppCompatActivity {
     public void buttonHandler(View view){
         outputText.setText(inputText.getText().toString());
     }
+    @SuppressLint("DefaultLocale")
     public void calculator(View view){
-        result.setText(String.format("%.2f", calcMean(values)));
+        result.setText(String.format("Medelvärde: %.2f\nMedian: %.2f\nStandardavvikelse: %.2f",
+                calcMean(values),
+                Statistics.calcMedian(values),
+                Statistics.calcStdev(values)
+        ));
     }
 
 }
--- app/src/main/java/fi/arcada/codechallenge/Statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -1,11 +1,37 @@
 package fi.arcada.codechallenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Statistics {
-    public static double calcMean(double[] array){
+
+
+    public static double calcStdev(ArrayList<Double> values){
+        double mean = Statistics.calcMean(values);
         double sum = 0;
-        for (double value : array) {
+        double varians = 0;
+        double dev = 0;
+
+        for(int i = 0; i < values.size(); i++){
+            sum += Math.pow((values.get(i) - mean), 2);
+        }
+        varians = sum / values.size();
+        dev = Math.sqrt(varians);
+        return dev;
+    }
+    public static double calcMedian(ArrayList<Double> values){
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        Collections.sort(sorted);
+        int midIndex = sorted.size()/2;
+        return sorted.get(midIndex);
+    }
+
+    public static double calcMean(ArrayList<Double> values){
+        double sum = 0;
+        for (double value : values) {
             sum += value;
         }
-        return sum / array.length;
+        return sum / values.size();
     }
+
 }
