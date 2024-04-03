05a1cc49b936007284421e6c4e647705666464c5
roosmaxm <roosmaxm@arcada.fi>
2024-04-03 12:00:25 +0300

    Lektion 5

--- app/src/main/java/fi/arcada/codechallenge/DataItemViewAdapter.java
+++ app/src/main/java/fi/arcada/codechallenge/DataItemViewAdapter.java
@@ -1,12 +1,25 @@
 package fi.arcada.codechallenge;
 
+import android.content.Context;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class DataItemViewAdapter extends RecyclerView.Adapter<DataItemViewAdapter.DataItemViewHolder> {
+
+    ArrayList<DataItem> dataItems;
+    Context context;
+    public DataItemViewAdapter(Context context, ArrayList<DataItem> dataItems) {
+        this.context = context;
+        this.dataItems = dataItems;
+    }
+
     @NonNull
     @Override
     public DataItemViewAdapter.DataItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -15,17 +28,25 @@ public class DataItemViewAdapter extends RecyclerView.Adapter<DataItemViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull DataItemViewAdapter.DataItemViewHolder holder, int position) {
+        DataItem item = dataItems.get(position);
+        holder.itemName.setText(item.getName());
+        holder.itemValue.setText(String.format("%.2f", item.getValue()));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataItems.size();
     }
 
     public class DataItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView itemName;
+        TextView itemValue;
         public DataItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemName = itemView.findViewById(R.id.dataItemName);
+            itemValue = itemView.findViewById(R.id.dataItemValue);
         }
     }
 }
