534b18d1b95202c4255a83694d5a0f4d2915c765
Max <roosmaxm@arcada.fi>
2024-04-09 14:03:23 +0300

    Kvartiler

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -3,7 +3,9 @@ package fi.arcada.codechallenge;
 import static fi.arcada.codechallenge.Statistics.calcMean;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     TextView outputText;
     EditText inputText;
     RecyclerView recyclerView;
+    SharedPreferences prefs;
+    SharedPreferences.Editor prefsEditor;
+    SharedPreferences counter;
+
     String[] names = {"Max", "Oliver", "Ben", "Alva", "Netta", "Olivia", "Morris", "Benjamin"};
     double [] testData = {12, 3, 53.2 ,7, 65, 55, 43, 21};
     ArrayList<DataItem> dataItems = new ArrayList<>();
@@ -39,13 +45,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         result = findViewById(R.id.result);
 
+        // SharedPreference
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Öppna editorn
+        prefsEditor = prefs.edit();
+
+        // Läs värde från SharedPreferences
+        int appStartCount = prefs.getInt("counter", 0);
+
+        // Lägg till värde till SharedPreferences och öka med 1
+        prefsEditor.putInt("counter", appStartCount + 1);
+
+        // Spara ändringen
+        prefsEditor.apply();
+
+        // Visa värdet
+        outputText.setText("Times you have opened app: " + appStartCount);
+
+
         //Vi fyller vår arraylist med värdena från testData
         for (int i = 0; i < testData.length; i++){
             dataItems.add(new DataItem(names[i], testData[i]));
         }
 
-
-        outputText.setText("");
 
         DataItemViewAdapter adapter = new DataItemViewAdapter(this, dataItems);
         recyclerView.setAdapter(adapter);
@@ -54,7 +77,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void buttonHandler(View view){
-        outputText.setText(inputText.getText().toString());
+        String text = inputText.getText().toString();
+        outputText.setText(text);
+
+        // Lägg till värde till SharedPreferences
+        SharedPreferences.Editor prefsEditor;
+        prefsEditor = prefs.edit();
+        prefsEditor.putString("lastText", text);
+        prefsEditor.apply();
     }
     @SuppressLint("DefaultLocale")
     public void calculator(View view){
